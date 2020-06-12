396747518f1bf14ea7694a556af781c28df0ec84
Resha Puspita <reshapuspita734@gmail.com>
2020-06-12 22:38:07 +0700

    debug done

--- index.js
+++ index.js
@@ -1,22 +1,33 @@
-const timIT = ["Yusuf", "Maffed", "Yusron", "Nadya", "Jessica",
-                "Adini", "Resha", "Petra", "David", "Nathanael"]
+const timIT = [
+  "Yusuf",
+  "Maffed",
+  "Yusron",
+  "Nadya",
+  "Jessica",
+  "Adini",
+  "Resha",
+  "Petra",
+  "David",
+  "Nathanael",
+];
 
 console.log(timIT.join());
-                
-const tim1 = timIT.splice(0,5)
-const tim2 = timIT
+
+const tim1 = timIT.splice(0, 5);
+const tim2 = timIT;
 // console.log(tim1);
 // console.log(tim2);
 
-// Tim 1
-let tim1list = tim1.map((tim) => `<li>${tim}</li>`)
+addList = (noTim, idKetua, timName, idElement) => {
+  document.getElementById(
+    `${idKetua}`
+  ).innerHTML = `Ketua Tim ${noTim}: ${timName.shift()}`;
+  let timList = "";
+  timName.forEach((arr) => {
+    timList = timList.concat(`<li>${arr}</li>`);
+  });
+  document.getElementById(`${idElement}`).innerHTML = timList;
+};
 
-document.getElementById("ketua1").innerHTML = `Ketua Tim 1: ${tim1.shift()}`
-document.getElementById("list-tim1").innerHTML = tim1list
-
-// Tim 2
-let tim2list = tim2.map((tim) => `<li>${tim}</li>`)
-
-document.getElementById("ketua2").innerHTML = `Ketua Tim 2: ${tim1.shift()}`
-document.getElementById("list-tim2").innerHTML = tim2list
-
+addList(1, "ketua1", tim1, "list-tim1");
+addList(2, "ketua2", tim2, "list-tim2");
